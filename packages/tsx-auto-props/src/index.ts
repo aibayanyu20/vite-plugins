15fdf97bb08919e6d4271b3f6f3c1d592edee49d
aibayanyu <aibayanyu@qq.com>
2023-11-27 22:43:09 +0800

    fix: enforce mode

--- packages/tsx-auto-props/src/index.ts
+++ packages/tsx-auto-props/src/index.ts
@@ -4,6 +4,7 @@ import { transform } from './parser'
 export function tsxAutoProps(): PluginOption {
   return {
     name: 'tsx-auto-props',
+    enforce: 'pre',
     transform(code, id) {
       if (id.endsWith('.tsx'))
         return transform(code, id)
