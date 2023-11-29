fc78f485deef5162550e7bc2d3aac176deed2421
aibayanyu <aibayanyu@qq.com>
2023-11-29 22:42:04 +0800

    test: add generic test

--- packages/tsx-auto-props/tests/fixtures/generic.tsx
+++ packages/tsx-auto-props/tests/fixtures/generic.tsx
@@ -1,7 +1,21 @@
 import { defineComponent, ref } from 'vue'
+import type { Props } from './typing'
 
 export const Generic = defineComponent(
     <T extends string | number>(props: { msg: T; list: T[] }) => {
+      const count = ref(0)
+
+      return () => (
+        <div>
+          {props.msg}
+          {' '}
+          {count.value}
+        </div>
+      )
+    },
+)
+export const Generic1 = defineComponent(
+    <T extends Props | number>(props: { msg: T; list: T[] }) => {
       const count = ref(0)
 
       return () => (
