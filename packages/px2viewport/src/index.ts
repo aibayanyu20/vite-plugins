c40a51a6b7942cb4f94a2750ab18839ed6c48a9e
aibayanyu20 <aibayanyu@qq.com>
2024-07-24 08:41:04 +0800

    fix: fix inline style is not support

--- packages/px2viewport/src/index.ts
+++ packages/px2viewport/src/index.ts
@@ -1,19 +1,25 @@
-import type { PluginOption } from 'vite'
+import type { FilterPattern, PluginOption } from 'vite'
+import { createFilter } from 'vite'
 import { transform } from './transform'
 
 export interface Px2viewportOptions {
   viewportWidth?: number
+  include?: FilterPattern
 }
 
 export function px2viewport(options: Px2viewportOptions = {
   viewportWidth: 750,
+  include: [/\.vue$/, /\.[jt]sx$/],
 }): PluginOption {
+  const filter = createFilter(options.include)
   return {
     name: 'px2viewport',
     transform(code, id) {
-      const res = transform(code, id, options.viewportWidth ?? 750)
-      if (res)
-        return res
+      if (filter(id)) {
+        const res = transform(code, options.viewportWidth ?? 750)
+        if (res)
+          return res
+      }
     },
   }
 }
--- packages/px2viewport/src/transform.ts
+++ packages/px2viewport/src/transform.ts
@@ -1,37 +1,36 @@
 import { findStaticImports } from 'mlly'
 import { MagicString } from 'magic-string-ast'
+import { inlineTransform } from './inlineTransform'
 
-export function transform(code: string, id: string, baseWidth = 750): undefined | any {
-  // 处理行内的样式
-  if (id.endsWith('.vue')) {
-    const m = new MagicString(code)
-    // 处理全局的参数来实现px2viewport
-    const imports = findStaticImports(code)
-    for (const source of imports) {
-      if (source.specifier === 'vue') {
-        // 判断里面是否包含 normalizeStyle as _normalizeStyle,
-        // 如果包含就去掉，然后用自己定义的 normalizeStyle
-        const _imports = source.imports
-        const normalizeStyleStr = 'normalizeStyle as _normalizeStyle'
-        if (_imports && _imports.includes(normalizeStyleStr)) {
-          let replaceCode = normalizeStyleStr
-          if (_imports.includes(`${normalizeStyleStr},`)) {
-            // 有逗号的情况
-            replaceCode = `${normalizeStyleStr},`
-          }
-          else if (_imports.includes(`, ${normalizeStyleStr}`)) {
-            // 有逗号的情况
-            replaceCode = `, ${normalizeStyleStr}`
-          }
-          const newImports = _imports.replace(replaceCode, '')
-          const replaceImport = `import ${newImports} from '${source.specifier}'\nimport { normalizeStyle as _normalizeStyle } from '@mistjs/vite-plugin-px2viewport/vue';\n_normalizeStyle.prototype.baseWidth=${baseWidth};\n`
-          m.overwrite(source.start, source.end, replaceImport)
+export function transform(code: string, baseWidth = 750): undefined | any {
+  code = inlineTransform(code)
+  const m = new MagicString(code)
+  // 处理全局的参数来实现px2viewport
+  const imports = findStaticImports(code)
+  for (const source of imports) {
+    if (source.specifier === 'vue') {
+      // 判断里面是否包含 normalizeStyle as _normalizeStyle,
+      // 如果包含就去掉，然后用自己定义的 normalizeStyle
+      const _imports = source.imports
+      const normalizeStyleStr = 'normalizeStyle as _normalizeStyle'
+      if (_imports && _imports.includes(normalizeStyleStr)) {
+        let replaceCode = normalizeStyleStr
+        if (_imports.includes(`${normalizeStyleStr},`)) {
+          // 有逗号的情况
+          replaceCode = `${normalizeStyleStr},`
         }
+        else if (_imports.includes(`, ${normalizeStyleStr}`)) {
+          // 有逗号的情况
+          replaceCode = `, ${normalizeStyleStr}`
+        }
+        const newImports = _imports.replace(replaceCode, '')
+        const replaceImport = `import ${newImports} from '${source.specifier}'\nimport { normalizeStyle as _normalizeStyle } from '@mistjs/vite-plugin-px2viewport/vue';\n_normalizeStyle.prototype.baseWidth=${baseWidth};\n`
+        m.overwrite(source.start, source.end, replaceImport)
       }
     }
-    return {
-      code: m.toString(),
-      map: m.generateMap({ hires: true }),
-    }
+  }
+  return {
+    code: m.toString(),
+    map: m.generateMap({ hires: true }),
   }
 }
