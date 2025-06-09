8e9c0e04182e9fe003081ac8ae7360f6903a2517
aibayanyu20 <aibayanyu@qq.com>
2025-06-09 09:31:28 +0800

    test: add filter test

--- packages/px2viewport/tests/filter.test.ts
+++ packages/px2viewport/tests/filter.test.ts
@@ -7,4 +7,10 @@ describe('test filter', () => {
     const id = `ss/test.jsx`
     expect(filter(id)).toBe(true)
   })
+  it('exclude test', () => {
+    const file = 'aaa/src/components/ViewTitle.vue?vue&type=script&lang.ts'
+    const filter = createFilter([/\.vue$/, /\.[jt]sx$/], [/node_modules/, 'src/components/ViewTitle.vue'])
+    const bool = filter(file)
+    expect(bool).toBe(false)
+  })
 })
