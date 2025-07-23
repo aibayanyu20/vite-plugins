bc4cdfae46799bef29156a91571f3b70f9d2a193
aibayanyu20 <aibayanyu@qq.com>
2025-07-23 08:37:12 +0800

    feat: add mock -h3

---
+++ packages/mock-h3/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  dts: true,
+  format: ['esm', 'cjs'],
+  clean: true,
+  external: [
+    'vite',
+  ],
+})
